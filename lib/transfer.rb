f15e303a6e90717271be8573608875dddf4d0c26
Amanda Ackerman <ackerm44@gmail.com>
2017-11-02 17:25:20 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,13 +14,13 @@ class Transfer
 
   def execute_transaction
     if self.status != "complete"
-      puts valid?
       if valid?
         sender.balance -= @amount
         receiver.balance += @amount
         @status = "complete"
       else
         "Transaction rejected. Please check your account balance."
+        @status = "rejected"
       end
     end
   end
