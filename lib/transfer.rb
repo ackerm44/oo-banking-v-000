2b94617e5d10098dd283c8a6759f6878c415aa5d
Amanda Ackerman <ackerm44@gmail.com>
2017-11-02 17:18:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,10 @@ class Transfer
   end
 
   def execute_transaction
-    sender.balance -= @amount
-    receiver.balance += @amount
+    if self.status != "complete"
+      sender.balance -= @amount
+      receiver.balance += @amount
+      @status = "complete"
+    end
   end
 end
